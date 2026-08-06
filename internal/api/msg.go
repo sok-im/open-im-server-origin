40ad692c902dcda73f179cfc32316074fe59aa77
hawklin2017 <32898629+hawklin2017@users.noreply.github.com>
2026-08-06 12:59:14 +0800

    backup

--- internal/api/msg.go
+++ internal/api/msg.go
@@ -636,7 +636,7 @@ func (m *MessageApi) GetPaymentNotifications(c *gin.Context) {
 		Total:         total,
 		Notifications: items,
 	})
-	log.ZDebug(c, "GetPaymentNotifications", "total", total, "items", len(items))
+	log.ZDebug(c, "GetPaymentNotifications", "req", req, "total", total, "items", len(items))
 	return
 }
 
